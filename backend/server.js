3d5bd757d6192f0dd333a43b8216111eb7e6da6e
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2023-10-05 02:08:04 +0530

    Fix: index html render

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
-require("dotenv").config()
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
 
-const route = require("./route")
+const route = require("./route");
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 7000;
 
-app.use(cors())
-app.use(route)
+app.use(cors());
+app.use("/api",route);
 
-const connectDB = async() => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`DB connected!`)
-    }catch(err){}
-}
+app.use(express.static(path.join(__dirname, "../frontend")));
 
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`Server Running!`)
-})
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`DB connected!`);
+  } catch (err) {}
+};
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server Running!`);
+});
